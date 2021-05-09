2f2fe7707807ff2a9077f6ec8c2f9c9e5d9acf0d
Yugandhar Yelai <63305302+Tech-nransom@users.noreply.github.com>
2021-05-09 18:51:18 +0530

    database file edited
    added base to the file

--- testing.py
+++ testing.py
@@ -1,10 +1,38 @@
-import math
-class TrialM:
-	def printing(self):
-		print("This is working successfully")
+import mysql.connector
 
-	def powerfunc(self):
-		print(math.pow(2,3))
+class Database:
+	def __init__(self,username,password,database):
 
-	def sinefunc(self,theta):
-		print(math.sin(theta))
+		try:
+			mydb = mysql.connector.connect(
+			  host="localhost",
+			  user=username,
+			  password=password,
+			)
+		except Exception as e:
+			print("username or password is wrong")
+			exit(1)
+
+		mycursor = mydb.cursor()
+
+		try:
+			sql =f"use {database};"
+			mycursor.execute(sql)
+
+		except :
+			print("Database is Missing So Creating One")
+			sql = f"create database {database};"
+			mycursor.execute(sql)
+			mydb.commit()
+
+			sql = "create table customers(user_id INT(11) NOT NULL AUTO_INCREMENT, name varchar(30), address varchar(50), age int, PRIMARY KEY (user_id));" 
+			print(sql)
+			mycursor.execute(sql)
+			mydb.commit()
+
+			sql = "create table items(user_id INT(11) NOT NULL AUTO_INCREMENT, name varchar(30), address varchar(50), age int, PRIMARY KEY (user_id));" 
+			print(sql)
+			mycursor.execute(sql)
+			mydb.commit()
+
+		
