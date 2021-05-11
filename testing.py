3063cd6e7bcf3ac4be9272f82bd7254f1a996e67
Yugandhar Yelai <63305302+Tech-nransom@users.noreply.github.com>
2021-05-11 10:40:55 +0530

    added few methods
    added creation of database,tables
    added addition methods into tables

--- testing.py
+++ testing.py
@@ -2,37 +2,61 @@ import mysql.connector
 
 class Database:
 	def __init__(self,username,password,database):
+		self.username = username
+		self.password = password
+		self.database = database
+		self.mydb,status = self.checkIfAllright(username,password)
+		if status:
+			self.mycursor = self.mydb.cursor()
 
+			try:
+				sql =f"use {database};"
+				self.mycursor.execute(sql)
+
+			except :
+				print("Database is Missing So Creating One")
+				sql = f"create database {database};"
+				self.mycursor.execute(sql)
+				self.mydb.commit()
+
+			sql = "create table IF NOT EXISTS customers(user_id int NOT NULL, name varchar(30) NOT NULL, family_members int NOT NULL, PRIMARY KEY (user_id));" 
+			print(sql)
+			self.mycursor.execute(sql)
+			self.mydb.commit()
+
+			sql = "create table IF NOT EXISTS items(order_id INT(11) NOT NULL AUTO_INCREMENT, alloted_rice float NOT NULL, remaining_amount float NOT NULL, user_id int, PRIMARY KEY (order_id), FOREIGN KEY (user_id) REFERENCES customers(user_id) ON DELETE CASCADE ON UPDATE CASCADE);" 
+			print(sql)
+			self.mycursor.execute(sql)
+			self.mydb.commit()
+		else:
+			print("something went wrong")
+
+	def getRemainingRice(self,user_id):
+		sql = f"select remaining_amount from items where user_id = {user_id};"
+		self.mycursor.execute(sql)
+
+	def add_customer(self,name,family_members,user_id):
+		sql = f"insert into customers(name,family_members,user_id) values('{name}','{family_members}','{user_id}');"
+		self.mycursor.execute(sql)
+		self.mydb.commit()
+		print("Insertion successfull")
+
+	def add_items(self,alloted_rice,remaining_amount,user_id):
+		sql = f"insert into items(alloted_rice,remaining_amount,user_id) values('{alloted_rice}','{remaining_amount}','{user_id}');"
+		self.mycursor.execute(sql)
+		self.mydb.commit()
+		print("Insertion successfull")
+
+
+
+	def checkIfAllright(self,username,password):
 		try:
 			mydb = mysql.connector.connect(
 			  host="localhost",
 			  user=username,
-			  password=password,
+			  password=password
 			)
+			return mydb,True
 		except Exception as e:
 			print("username or password is wrong")
-			exit(1)
-
-		mycursor = mydb.cursor()
-
-		try:
-			sql =f"use {database};"
-			mycursor.execute(sql)
-
-		except :
-			print("Database is Missing So Creating One")
-			sql = f"create database {database};"
-			mycursor.execute(sql)
-			mydb.commit()
-
-			sql = "create table customers(user_id INT(11) NOT NULL AUTO_INCREMENT, name varchar(30), family_members varchar(50), PRIMARY KEY (user_id));" 
-			print(sql)
-			mycursor.execute(sql)
-			mydb.commit()
-
-			sql = "create table items(user_id INT(11) NOT NULL AUTO_INCREMENT, alloted_rice float, remaining_amount float, PRIMARY KEY (user_id));" 
-			print(sql)
-			mycursor.execute(sql)
-			mydb.commit()
-
-		
+			return [],False
